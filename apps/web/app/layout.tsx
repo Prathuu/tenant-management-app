76022c6462c6536a097c82f15bdffd17f1fa1cbd
Pitambar Jadhav <pitambar.creates@gmail.com>
2026-03-12 23:11:53 +0530

    feat: theme provide integration

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,27 @@
 import { ReactQueryProvider } from "@/providers/react-query-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+
 import { ReactNode } from "react";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html className={cn("font-sans", geist.variable)}>
+    <html
+      lang="en"
+      className={cn("font-sans", geist.variable)}
+      suppressHydrationWarning
+    >
       <body>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <ThemeProvider>
+          <ReactQueryProvider>
+            {children}
+            <Toaster />
+          </ReactQueryProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
