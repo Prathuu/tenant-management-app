88d39b895da70340db8aa942f5f5eaa27498fc8f
Pitambar Jadhav <pitambar.creates@gmail.com>
2026-03-12 22:56:09 +0530

    feat: installed next-themes and shadcn-ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,14 @@
 import { ReactQueryProvider } from "@/providers/react-query-provider";
 import { ReactNode } from "react";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html>
+    <html className={cn("font-sans", geist.variable)}>
       <body>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
