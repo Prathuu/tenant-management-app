2f0702299875f6f79c46c3363d05e0d2ec864820
Pitambar Jadhav <pitambar.creates@gmail.com>
2026-03-19 16:49:30 +0530

    feat: added rooms page

---
+++ apps/web/app/dashboard/rooms/page.tsx
@@ -0,0 +1,13 @@
+import { StatCard } from "@/components/shared/stat-card";
+
+export default function Page() {
+  return (
+    <div className="grid grid-cols-3 gap-6">
+      <StatCard title="Total Tenants" value="128" />
+
+      <StatCard title="Vacant Rooms" value="12" />
+
+      <StatCard title="Monthly Revenue" value="₹2,30,000" />
+    </div>
+  );
+}
