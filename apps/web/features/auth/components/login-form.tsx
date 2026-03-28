12ba35b45b79f52dd971c7fcd7b704defa63fec7
Pitambar Jadhav <pitambar.creates@gmail.com>
2026-03-28 23:55:11 +0530

    feat: login form validation ui added

--- apps/web/features/auth/components/login-form.tsx
+++ apps/web/features/auth/components/login-form.tsx
@@ -20,18 +20,34 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-      <GlassInput
-        placeholder="Email"
-        {...form.register("email")}
-        endIcon={<UserRound size={18} />}
-      />
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <div className={form.formState.errors.email ? "mb-1" : "mb-4"}>
+        <GlassInput
+          placeholder="Email"
+          {...form.register("email")}
+          endIcon={<UserRound size={18} />}
+          error={!!form.formState.errors.email}
+        />
+      </div>
+      {form.formState.errors.email && (
+        <p className="text-red-500 text-sm px-3 mb-1">
+          {form.formState.errors.email.message}
+        </p>
+      )}
 
-      <GlassInput
-        type="password"
-        placeholder="Password"
-        {...form.register("password")}
-      />
+      <div className={form.formState.errors.password ? "mb-1" : "mb-4"}>
+        <GlassInput
+          type="password"
+          placeholder="Password"
+          {...form.register("password")}
+          error={!!form.formState.errors.password}
+        />
+      </div>
+      {form.formState.errors.password && (
+        <p className="text-red-500 text-sm px-3 mb-1">
+          {form.formState.errors.password.message}
+        </p>
+      )}
 
       <GlassButton type="submit" className="mx-auto w-full" disabled={loading}>
         {loading ? "Signing in..." : "Login"}
