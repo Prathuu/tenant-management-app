8ccb30ba34493eb94afd8520b2c79b08161861f5
Pitambar Jadhav <pitambar.creates@gmail.com>
2026-03-28 23:59:33 +0530

    feat: register validation ui added

--- apps/web/features/auth/components/register-form.tsx
+++ apps/web/features/auth/components/register-form.tsx
@@ -27,27 +27,63 @@ export const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-      <GlassInput
-        placeholder="Name"
-        {...form.register("name")}
-        endIcon={<UserRound size={18} />}
-      />
-      <GlassInput
-        placeholder="Email"
-        {...form.register("email")}
-        endIcon={<Mail size={18} />}
-      />
-      <GlassInput
-        type="password"
-        placeholder="Password"
-        {...form.register("password")}
-      />
-      <GlassInput
-        type="password"
-        placeholder="Confirm Password"
-        {...form.register("confirmPassword")}
-      />
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <div className={form.formState.errors.name ? "mb-1" : "mb-4"}>
+        <GlassInput
+          placeholder="Name"
+          {...form.register("name")}
+          endIcon={<UserRound size={18} />}
+          error={!!form.formState.errors.name}
+        />
+      </div>
+      {form.formState.errors.name && (
+        <p className="text-red-500 text-sm px-3 mb-1">
+          {form.formState.errors.name.message}
+        </p>
+      )}
+      <div className={form.formState.errors.email ? "mb-1" : "mb-4"}>
+        <GlassInput
+          placeholder="Email"
+          {...form.register("email")}
+          endIcon={<Mail size={18} />}
+          error={!!form.formState.errors.email}
+        />
+      </div>
+
+      {form.formState.errors.email && (
+        <p className="text-red-500 text-sm px-3 mb-1">
+          {form.formState.errors.email.message}
+        </p>
+      )}
+
+      <div className={form.formState.errors.password ? "mb-1" : "mb-4"}>
+        <GlassInput
+          type="password"
+          placeholder="Password"
+          {...form.register("password")}
+          error={!!form.formState.errors.password}
+        />
+      </div>
+
+      {form.formState.errors.password && (
+        <p className="text-red-500 text-sm px-3 mb-1">
+          {form.formState.errors.password.message}
+        </p>
+      )}
+
+      <div className={form.formState.errors.confirmPassword ? "mb-1" : "mb-4"}>
+        <GlassInput
+          type="password"
+          placeholder="Confirm Password"
+          {...form.register("confirmPassword")}
+          error={!!form.formState.errors.confirmPassword}
+        />
+      </div>
+      {form.formState.errors.confirmPassword && (
+        <p className="text-red-500 text-sm px-3 mb-1">
+          {form.formState.errors.confirmPassword.message}
+        </p>
+      )}
 
       <GlassButton type="submit" className="w-full" disabled={loading}>
         {loading ? "Creating account..." : "Register"}
